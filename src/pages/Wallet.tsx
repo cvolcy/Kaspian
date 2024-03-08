c5842e9a7e3106f48c313a7235ff57e5beb7c8f6
Clervens Volcy <clervens.volcy@gmail.com>
2024-03-08 14:07:09 -0500

    Make a copy address link for usableAddress

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { Download, LogOutIcon } from "lucide-react"
+import { CopyIcon, CopyCheckIcon, LogOutIcon } from "lucide-react"
 import Heading from "@/components/Heading"
 // import UTXOCard from "@/components/UTXOCard"
 import SendDrawer from "@/pages/Wallet/Send"
@@ -7,7 +7,7 @@ import ReceiveDrawer from "@/pages/Wallet/Receive"
 import SettingsSheet from "@/pages/Wallet/Settings"
 import { i18n } from "webextension-polyfill"
 import useKaspa from "@/hooks/useKaspa"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Status } from "@/wallet/kaspa/wallet"
 import { useNavigate } from "react-router-dom"
 import { Textarea } from "@/components/ui/textarea"
@@ -15,6 +15,7 @@ import { Textarea } from "@/components/ui/textarea"
 export default function Wallet () {
   const kaspa = useKaspa()
   const navigate = useNavigate()
+  const [addressCopied, setAddressCopied] = useState(false)
 
   useEffect(() => {
     if (kaspa.status !== Status.Unlocked) {
@@ -41,15 +42,26 @@ export default function Wallet () {
       </div>
       <div className={"flex flex-col gap-1"}>
         <div className={"flex flex-col items-center"}>
+          <Button
+            className={"flex flex-row text-sm font-semibold"}
+            title={kaspa.usableAddress}
+            variant={"link"}
+            onClick={(e) => {
+              e.preventDefault()
+              navigator.clipboard.writeText(kaspa.usableAddress)
+              setAddressCopied(true)
+              setTimeout(() => setAddressCopied(false), 5000)
+            }}
+          >
+            {kaspa.usableAddress.substring(0, 14)}...{kaspa.usableAddress.substring(kaspa.usableAddress.length - 7)}
+            {
+              addressCopied
+              ? <CopyCheckIcon className={"h-4 w-4 ml-1"} />
+              : <CopyIcon className={"h-4 w-4 ml-1"} />
+            }
+          </Button>
           <p className={"text-4xl font-extrabold"}>{kaspa.balance}</p>
           <p className={"text-xl font-bold"}>$ 0.00</p>
-        </div>
-        <div className={"flex flex-col items-center"}>
-          <Textarea
-            defaultValue={kaspa.usableAddress}
-            className={"w-72 resize-none"}
-            disabled={true}
-          />
         </div>
       </div>
       <div className={"grid grid-cols-3 mx-3 h-full overflow-y-scroll no-scrollbar gap-2"}>
